74c716ec3b3f587fb0e74d79e467d02feef681fb
Edgar Neto <edgar.figueiredo@gmail.com>
2013-07-03 14:31:54 +0200

    Stuff falling

--- Assets/Scripts/TrackBehavior.cs
+++ Assets/Scripts/TrackBehavior.cs
@@ -1,15 +1,22 @@
 using UnityEngine;
 using System.Collections;
 
-public class TrackBehavior : MonoBehaviour {
+public class TrackBehavior : MonoBehaviour
+{
+
+	private Transform _transform;
+
+	public float Speed;
 
 	// Use this for initialization
-	void Start () {
-	
+	void Start ()
+	{
+		_transform = this.transform;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate ()
+	{
+		_transform.Translate(0,(-Speed/100),0);
 	}
 }
