3f7d6a8d18886b04c103d252373adccf2b0c3397
Edgar Neto <edgar.figueiredo@gmail.com>
2013-07-03 14:52:31 +0200

    Detecting Taps

---
+++ Assets/Scripts/Input.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+using System.Collections;
+
+public class Input : MonoBehaviour
+{
+	void OnTap(TapGesture gesture)
+	{
+		var middle = Screen.width/2;
+		if (gesture.Position.x <= middle)
+		{
+			Debug.Log("Left tap");
+		}
+		else
+		{
+			Debug.Log("Right tap");
+		}
+	}
+}
